1c2f73f304f91df92b04bfae8e3c466c0574a0d0
yusrililhm <yusrililham62@gmail.com>
2023-12-09 20:09:51 +0700

    belajar docker expose

---
+++ belajar_docker_file/expose/go.mod
@@ -0,0 +1,3 @@
+module go_docker_expose
+
+go 1.21.4
--- belajar_docker_file/expose/main.go
+++ belajar_docker_file/expose/main.go
@@ -1,5 +1,19 @@
 package main
 
+import (
+	"log"
+	"net/http"
+)
+
 func main() {
-	
+	server := http.Server{
+		Addr: ":8080",
+	}
+
+	log.Printf("server is running on port %s\n", server.Addr)
+	err := server.ListenAndServe()
+
+	if err != nil {
+		log.Fatalf("something went wrong: %s", err.Error())
+	}
 }
